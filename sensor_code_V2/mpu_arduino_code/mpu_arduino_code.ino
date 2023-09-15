b2276234e0119cb92a5d5f2908d55417c613ade1
Sefa Sayracı <73780930+Sefasayraci@users.noreply.github.com>
2023-09-15 13:59:27 +0300

    Add files via upload

---
+++ sensor_code_V2/mpu_arduino_code/mpu_arduino_code.ino
@@ -0,0 +1,30 @@
+#include "I2Cdev.h" //I2C kütüphanesi
+#include "MPU6050.h" //Mpu6050 kütüphanesi
+#include "Wire.h"
+MPU6050 accelgyro; // Mpu6050 sensör tanımlama
+int16_t ax, ay, az; //ivme tanımlama
+int16_t gx, gy, gz; //gyro tanımlama
+  
+void setup() {
+Wire.begin();
+Serial.begin(9600);
+//Serial.println("I2C cihazlar başlatılıyor...");
+accelgyro.initialize();
+//Serial.println("Test cihazı bağlantıları...");
+//Serial.println(accelgyro.testConnection() ? "MPU6050 bağlantı başarılı" : "MPU6050 bağlantısı başarısız");
+}
+  
+void loop() {
+accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); // ivme ve gyro değerlerini okuma
+  
+//açısal ivmeleri ve gyro değerlerini ekrana yazdırma
+//Serial.print("a/g:\t");
+Serial.print(ax); Serial.print("/");
+Serial.print(ay); Serial.print("/");
+Serial.print(az); Serial.print("/");
+Serial.print(gx); Serial.print("/");
+Serial.print(gy); Serial.print("/");
+Serial.println(gz);
+
+delay(1000);
+}
---
+++ sensor_code_V2/system_arduino_code_hmc_angle/system_code/system_code.ino
@@ -0,0 +1,154 @@
+/////////////////////////////////////////////
+// Sadece eklenecek olan pusula sensörü kaldı. O da lehimlenecek. Açılarda eklenebilir.
+////////////////////////////////////////////
+
+#include <Wire.h>
+#include <Adafruit_BMP085.h>  // Adafruit BMP085 library
+#include "I2Cdev.h" //I2C kütüphanesi
+#include "MPU6050.h" //Mpu6050 kütüphanesi
+#include "Wire.h"
+
+#define Magnetometer_mX0 0x03  
+#define Magnetometer_mX1 0x04  
+#define Magnetometer_mZ0 0x05  
+#define Magnetometer_mZ1 0x06  
+#define Magnetometer_mY0 0x07  
+#define Magnetometer_mY1 0x08
+
+#define Magnetometer 0x1E //I2C 7bit address of HMC5883
+
+Adafruit_BMP085 bmp;
+MPU6050 accelgyro; // Mpu6050 sensör tanımlama
+
+int16_t ax, ay, az; //ivme tanımlama
+int16_t gx, gy, gz; //gyro tanımlama
+int mX0, mX1, mX_out;
+int mY0, mY1, mY_out;
+int mZ0, mZ1, mZ_out;
+
+float heading, headingDegrees, headingFiltered, declination;
+float Xm,Ym,Zm;
+
+void setup(){
+
+ Wire.begin();
+   delay(100);
+   
+ Serial.begin(9600);
+ accelgyro.initialize();
+
+ if (!bmp.begin()) {
+  while (true) {}
+ }
+
+  Wire.beginTransmission(Magnetometer); 
+  Wire.write(0x02); // Select mode register
+  Wire.write(0x00); // Continuous measurement mode
+  Wire.endTransmission();
+  
+}
+
+void loop(){
+ accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  
+  //---- X-Axis
+  Wire.beginTransmission(Magnetometer); // transmit to device
+  Wire.write(Magnetometer_mX1);
+  Wire.endTransmission();
+  Wire.requestFrom(Magnetometer,1); 
+  if(Wire.available()<=1)   
+  {
+    mX0 = Wire.read();
+  }
+  Wire.beginTransmission(Magnetometer); // transmit to device
+  Wire.write(Magnetometer_mX0);
+  Wire.endTransmission();
+  Wire.requestFrom(Magnetometer,1); 
+  if(Wire.available()<=1)   
+  {
+    mX1 = Wire.read();
+  }
+  //---- Y-Axis
+  Wire.beginTransmission(Magnetometer); // transmit to device
+  Wire.write(Magnetometer_mY1);
+  Wire.endTransmission();
+  Wire.requestFrom(Magnetometer,1); 
+  if(Wire.available()<=1)   
+  {
+    mY0 = Wire.read();
+  }
+  Wire.beginTransmission(Magnetometer); // transmit to device
+  Wire.write(Magnetometer_mY0);
+  Wire.endTransmission();
+  Wire.requestFrom(Magnetometer,1); 
+  if(Wire.available()<=1)   
+  {
+    mY1 = Wire.read();
+  }
+  
+  //---- Z-Axis
+  Wire.beginTransmission(Magnetometer); // transmit to device
+  Wire.write(Magnetometer_mZ1);
+  Wire.endTransmission();
+  Wire.requestFrom(Magnetometer,1); 
+  if(Wire.available()<=1)   
+  {
+    mZ0 = Wire.read();
+  }
+  Wire.beginTransmission(Magnetometer); // transmit to device
+  Wire.write(Magnetometer_mZ0);
+  Wire.endTransmission();
+  Wire.requestFrom(Magnetometer,1); 
+  if(Wire.available()<=1)   
+  {
+    mZ1 = Wire.read();
+  }
+  
+  //---- X-Axis
+  mX1=mX1<<8;
+  mX_out =mX0+mX1; // Raw data
+  // From the datasheet: 0.92 mG/digit
+  Xm = mX_out*0.00092; // Gauss unit
+  //* Earth magnetic field ranges from 0.25 to 0.65 Gauss, so these are the values that we need to get approximately.
+  //---- Y-Axis
+  mY1=mY1<<8;
+  mY_out =mY0+mY1;
+  Ym = mY_out*0.00092;
+  //---- Z-Axis
+  mZ1=mZ1<<8;
+  mZ_out =mZ0+mZ1;
+  Zm = mZ_out*0.00092;
+  // ==============================
+  //Calculating Heading
+  heading = atan2(Ym, Xm);
+ 
+  // Correcting the heading with the declination angle depending on your location
+  // You can find your declination angle at: https://www.ngdc.noaa.gov/geomag-web/
+  // At my location it's 4.2 degrees => 0.073 rad
+  declination = 0.073; 
+  heading += declination;
+  
+  // Correcting when signs are reveresed
+  if(heading <0) heading += 2*PI;
+  // Correcting due to the addition of the declination angle
+  if(heading > 2*PI)heading -= 2*PI;
+  headingDegrees = heading * 180/PI; // The heading in Degrees unit
+  // Smoothing the output angle / Low pass filter 
+  headingFiltered = headingFiltered*0.85 + headingDegrees*0.15;
+  //Sending the heading value through the Serial Port to Processing IDE
+ //Serial.println(headingFiltered);
+  
+  delay(50);
+ Serial.print(bmp.readTemperature());Serial.print("/");
+ Serial.print(bmp.readPressure());Serial.print("/");
+ Serial.print(bmp.readAltitude());Serial.print("/");
+ Serial.print(ax); Serial.print("/");
+ Serial.print(ay); Serial.print("/");
+ Serial.print(az); Serial.print("/");
+ Serial.print(gx); Serial.print("/");
+ Serial.print(gy); Serial.print("/");
+ Serial.print(gz); Serial.print("/");
+ Serial.println(headingFiltered);
+ 
+ delay(1000);
+}
